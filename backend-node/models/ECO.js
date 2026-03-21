44bdaa0bce4eb48b1d3c0b585a419c776400b2ee
Parth Kachariya <parthkachariya1234@gmail.com>
2026-03-21 13:28:58 +0530

    auth.js

--- backend-node/models/ECO.js
+++ backend-node/models/ECO.js
@@ -3,6 +3,7 @@
 // =============================================================================
 const { DataTypes } = require('sequelize');
 
+
 module.exports = (sequelize) =>
   sequelize.define('ECO', {
     id: {
